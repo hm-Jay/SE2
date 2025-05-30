791ee65551df3cfe5ea85c005e40e2da74befc99
popat <hm-popat@hm.edu>
2025-05-30 14:51:37 +0200

    040 Teil 1 erledigt

---
+++ Uebung040/src/HMResource.java
@@ -0,0 +1,25 @@
+public abstract class HMResource {
+    private final String id;
+    private final String titel;
+
+    public HMResource(String id, String titel) {
+        if (id == null || titel == null) {
+            throw new IllegalArgumentException("ID und Titel dürfen nicht null sein.");
+        }
+
+        this.id = id;
+        this.titel = titel;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getTitel() {
+        return titel;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + titel;
+    }
+}
